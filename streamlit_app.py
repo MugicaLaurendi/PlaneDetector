397bc4ad34b5fd2a5d1c5452b40c6c99c67b5a36
MugicaLaurendi <laurendi.mugica@gmail.com>
2024-03-13 18:46:17 +0100

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,46 @@
-import altair as alt
-import pandas as pd
 import streamlit as st
-import requests
-"""
-# Bienvenue sur mon API Wine
+from rembg import remove
+from PIL import Image
+from io import BytesIO
+import base64
 
-Ce streamlit est un exercice
+st.set_page_config(layout="wide", page_title="Image Background Remover")
 
-"""
+st.write("## Remove background from your image")
+st.write(
+    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+)
+st.sidebar.write("## Upload and download :gear:")
 
-alcohol = st.slider('Alcohol', 8.0, 15.0, 10.0,step=0.1)
-sulphates = st.slider('Sulphates', 0.0, 3.0, 1.0,step=0.1)
-citric_acid = st.slider('Citric acid', 0.0, 1.0, 0.5,step=0.01)
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
-response = requests.get(f"https://1f07-82-126-158-181.eu.ngrok.io/lr_red_wine?alcohol={alcohol}&sulphates={sulphates}&citric_acid={citric_acid}").json()
+# Download the fixed image
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
 
-quality = round(response["Wine quality"])
 
-st.write(f"## Wine quality : {quality}/10")
+def fix_image(upload):
+    image = Image.open(upload)
+    col1.write("Original Image :camera:")
+    col1.image(image)
+
+    fixed = remove(image)
+    col2.write("Fixed Image :wrench:")
+    col2.image(fixed)
+    st.sidebar.markdown("\n")
+    st.sidebar.download_button("Download fixed image", convert_image(fixed), "fixed.png", "image/png")
+
+
+col1, col2 = st.columns(2)
+my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+if my_upload is not None:
+    if my_upload.size > MAX_FILE_SIZE:
+        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
+    else:
+        fix_image(upload=my_upload)
+else:
+    fix_image("./zebra.jpg")
